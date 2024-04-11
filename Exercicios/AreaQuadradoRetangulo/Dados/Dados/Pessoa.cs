69099a55dada4083edac18a568f482e8bf0d5e2f
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-04-11 12:12:30 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/Dados/Dados/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    internal class Pessoa
+    {
+        private string? Nome { get; set; }
+        private DateTime Nascimento { get; set; }
+        private Double Altura { get; set; }
+
+        private int Idade { get; set; }
+        public string Info()
+        {
+            Nome = "Enzo Caetano";
+            Nascimento =  new DateTime(2007, 11, 24, 11,30,00);
+            Altura = Convert.ToDouble(1.73);
+
+            return $"Nome: {Nome}\nNAscimento: {Nascimento}\nAltura: {Altura}";
+        }
+
+        public string Calcularidade()
+        {
+            int Ano = 2024;
+            Idade = Ano - Nascimento.Year;
+
+
+            return $"Idade: {Idade}";
+
+        }
+
+
+    }
+}
