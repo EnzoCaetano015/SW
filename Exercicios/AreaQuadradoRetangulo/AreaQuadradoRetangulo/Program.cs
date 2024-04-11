abe106d039f4ea7194fde1ef8494a58d74810be1
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-04-11 11:02:14 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Calcula Área Quadrado/Retângulo");
+            Area area = new Area();
+
+            Console.WriteLine(area.Calcular());
+            Console.WriteLine(area.Dados());
+        }
+    }
+}
