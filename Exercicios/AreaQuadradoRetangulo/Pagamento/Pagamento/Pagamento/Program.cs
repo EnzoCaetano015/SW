f6b4dabd765396a268116fecdb54d21b8b1811d0
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-04-12 16:43:55 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/Pagamento/Pagamento/Pagamento/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Pagamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FPagamento pagamento = new FPagamento();
+
+            Console.WriteLine(pagamento.Pag());
+        }
+    }
+}
