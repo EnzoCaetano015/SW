647eeda53ebfafe31ae48a4e3124fc05208a7b8c
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-04-12 16:43:14 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/Album/AlbumFotos/AlbumFotos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AlbumFotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando numero de paginas:");
+            Album album = new Album();
+
+            Console.WriteLine(album.Pagina());
+        }
+    }
+}
