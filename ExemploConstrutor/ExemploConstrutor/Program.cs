0cf14946b9bb01207d41a5ec46def02df7780083
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-04-25 12:17:42 -0300

    Add files via upload

---
+++ ExemploConstrutor/ExemploConstrutor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploConstrutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo construtor");
+
+            Cliente cliente = new Cliente("Enzo", 12345);
+
+           //cliente.Nome = "Enzo Caetano";
+            //cliente.Email = "email@email.com";
+            //cliente.Senha = 1234;
+
+            cliente.MostraDados();
+        }
+    }
+}
