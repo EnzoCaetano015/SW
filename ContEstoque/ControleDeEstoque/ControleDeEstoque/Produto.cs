eb0e989fae97821602149e80859cfd7af63e14bd
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-04-26 23:21:49 -0300

    Add files via upload

---
+++ ContEstoque/ControleDeEstoque/ControleDeEstoque/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque
+{
+    public class Produto
+    {
+        private int code {  get; set; }
+        private string? nome { get; set; }
+
+        private int estoque { get; set; }
+        private double preco { get; set; }
+
+        public void entrada(int quant)
+        {
+            estoque += quant;
+            Console.WriteLine("Entrada: " + quant);
+        }
+
+        public void venda(int quant)
+        {
+            estoque -= quant;
+            Console.WriteLine("Venda: " + quant);
+        }
+
+        public void saida()
+        {
+            Console.WriteLine("Informações do Produto");
+            Console.WriteLine("Código: " + code);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Estoque: " + estoque);
+            Console.WriteLine("Preço: " + preco);
+        }
+
+        public Produto(int codigo, string nomeCons, int estoqueCons, double precoCons)
+        {
+            code = codigo;
+            nome = nomeCons;
+            estoque = estoqueCons;
+            preco = precoCons;
+
+        }
+
+    }
+}
---
+++ ContEstoque/ControleDeEstoque/ControleDeEstoque/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ControleDeEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto(336,"Geladeira",0,4500);
+            produto.saida();
+            produto.entrada(10);
+            produto.venda(8);
+            produto.saida();
+        }
+    }
+}
